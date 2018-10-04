0a2ff39124c3e3cd48fd94a325f7fbc344116015
Leo Torres <leohtor@gmail.com>
2018-10-04 10:49:49 -0700

    Added Header styled components.

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import styled from 'styled-components'
+
+const HeaderWrapper = styled.div`
+  background: inherit;
+  marginBottom: 1.45rem;
+`
+
+const HeaderContainer = styled.div`
+  margin: 0 auto;
+  max-width: 90vw;
+  padding: 1.45rem 1.0875rem;
+`
+
+
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'inherit',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
+  <HeaderWrapper>
+    <HeaderContainer>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
@@ -40,8 +43,8 @@ const Header = ({ siteTitle }) => (
           <Link to="/portfolio/">Portfolio</Link>
         </li>
       </ul>
-    </div>
-  </div>
+    </HeaderContainer>
+  </HeaderWrapper>
 )
 
 export default Header
