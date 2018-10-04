ba6c380996290f7dd36c0f76a0f74406e329b873
Leo Torres <leohtor@gmail.com>
2018-10-04 11:17:14 -0700

    Added initial header ul styles.

--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,48 @@ import { Link } from 'gatsby'
 import styled from 'styled-components'
 
 const HeaderWrapper = styled.div`
+  position: relative;
+  display: inline-flex;
+  max-width: 100%;
+  // padding: 1.45rem 0rem 1.45rem;
   background: inherit;
   marginBottom: 1.45rem;
+
+  h1 {
+    font-weight: 500;
+    font-size: 2.25rem;
+    a {
+      text-decoration: none;
+      color: #1f2023;
+      text-shadow: 0.5px 0.5px 1px #636363, -0.25px -0.25px 2px #636363;
+      padding-left: 40px;
+    }
+  }
+  p {
+    color: #4d4d4d;
+  }
+
+  ul {
+    display: inline-flex;
+    margin-right: 20px;
+    li {
+      list-style: none;
+      margin-right: 20px;
+      padding-top: 12px;
+      font-size: 1.3rem;
+    }
+  }
+  a {
+    color: #FF7C00;
+    text-decoration: none;
+    &:hover {
+      transition: all 230ms ease;
+      border-bottom: 5px solid cornflowerblue;
+    }
+  }
+  .active {
+    border-bottom: 5px solid cornflowerblue;
+  }
 `
 
 const HeaderContainer = styled.div`
@@ -13,22 +53,45 @@ const HeaderContainer = styled.div`
   padding: 1.45rem 1.0875rem;
 `
 
+const LogoContainer = styled.div`
+  min-width: 75vw;
+`
+
+const NavContainer = styled.div`
+  z-index: 3;
+  min-width: 25vw;
+  margin: 0;
+
+  a {
+    &:hover {
+      transition: all 230ms ease;
+      border-bottom: 5px solid cornflowerblue;
+    }
+  }
+  .active {
+    border-bottom: 5px solid cornflowerblue;
+  }
+`
+
 
 
 const Header = ({ siteTitle }) => (
   <HeaderWrapper>
     <HeaderContainer>
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: '#1f2023',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+      <LogoContainer>
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: '#1f2023',
+              textDecoration: 'none',
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+      </LogoContainer>
+      <NavContainer>
       <ul>
         <li>
           <Link to="/">Home</Link>
@@ -43,6 +106,7 @@ const Header = ({ siteTitle }) => (
           <Link to="/portfolio/">Portfolio</Link>
         </li>
       </ul>
+      </NavContainer>
     </HeaderContainer>
   </HeaderWrapper>
 )
