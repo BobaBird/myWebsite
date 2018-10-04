e50f45413fd0b2ef2048267557b0a2c2da415f0e
Leo Torres <leohtor@gmail.com>
2018-10-04 08:54:30 -0700

    Set footer basic styles.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
 
-
+const SectionContainer = styled.div`
+    
+`
 
 
 const Footer = () => (
   <div
     style={{
-        minHeight: '120px',
+        minHeight: '380px',
         backgroundColor: 'rgba(20, 20, 20, 1)',
         marginBottom: '0',
     }}
   >
+    <SectionContainer>
+
+    </SectionContainer>
   </div>
 )
 
